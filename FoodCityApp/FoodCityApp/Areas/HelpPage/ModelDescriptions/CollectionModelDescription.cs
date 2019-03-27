9fe7d812fb420faf07702533b96432a5fbb80103
kanushtika <kanushtika.mahendran@mcmedisoft.com>
2019-03-27 12:22:01 +0530

    Added food city application

---
+++ FoodCityApp/FoodCityApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FoodCityApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FoodCityApp/FoodCityApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FoodCityApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
